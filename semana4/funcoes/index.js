c61262e9b55093e1c2c5e6e64192225d466efd88
papirusgx <michael_nickolas@hotmail.com>
2020-10-05 16:53:35 -0300

    Update 05/10/20

---
+++ semana4/funcoes/index.js
@@ -0,0 +1,97 @@
+// // 1 a
+// 10
+// 50
+
+// // b
+// nenhuma mensagem no console
+
+// 2. a
+// Darvas 
+// Caio
+
+// // b.
+// Amanda 
+// Caio
+
+// 3.
+// Define uma função que identifica os valores pares pare de um array
+
+
+// 4. a
+// const mensagem = () => {
+//   console.log('Eu sou Michael,tenho 26 anos, moro em Sergipe e sou estudante.')
+  
+// }
+
+// mensagem()
+
+// b
+// const pessoa = (nome,idade,cidade,boolean) => {
+ 
+// idade = Number
+
+//   nome = prompt('Digite seu nome')
+//   idade = prompt('Digite sua idade')
+// cidade = prompt ('Digite sua cidade')
+// boolean = prompt ('Vc é estudante? digite [ OK para sim/ Cancelar para nao]')
+
+// if( boolean === true){
+//  boolean = ' Sou '
+// }else{
+//   boolean = ' Não sou '
+// }
+
+// return ('Eu sou ' + nome + ' ,tenho ' + idade + ' anos,moro em ' +  cidade  +  boolean + ' estudante ')
+
+// }
+// console.log(pessoa())
+
+// 5.a
+
+// function Soma(a, b) {
+//   console.log( a + b);
+  
+// }
+
+// Soma(4,10)
+
+// b
+
+// function MenorouMaior(a, b) {
+//   if(a > b) {
+//     console.log('A é maior que B')
+//   }else if (a === b) {
+//     console.log('A é igual a B')
+//   }else{
+//     console.log('A é menor que B')
+//   }
+// }
+
+// MenorouMaior(11,10)
+
+// c.
+
+// let mostrarMensagem = () => {       
+//   for(let i = 0; i < 11 ; i++) {
+//     const meuArray = ['Hello World' ]
+//     console.log(meuArray)
+//   }
+  
+// }
+
+// let int = mostrarMensagem()
+
+// 6. a
+const array = () => {
+
+
+  
+}
+
+
+
+ 
+ 
+
+
+

cf9b8426ca61412c16561f295312eebc0d64799b
papirusgx <michael_nickolas@hotmail.com>
2020-10-06 20:44:35 -0300

    Incompleto

--- semana4/Dom/index.js
+++ semana4/Dom/index.js
@@ -1,3 +1,69 @@
+
+function adicionaTarefa() {
+  const meuInput = document.getElementById("meu-input");
+
+  let divdomingo = document.getElementById("domingo");
+  let divsegunda = document.getElementById("segunda");
+  let divterca = document.getElementById("terca");
+  let divquarta = document.getElementById("quarta");
+  let divquinta = document.getElementById("quinta");
+  let divsexta = document.getElementById("sexta");
+  let divsabado = document.getElementById("sabado");
+
+  let diaEscolhido = document.getElementById("semana");
+
+  switch (diaEscolhido) {
+    case divsegunda:
+      const minhaLista = document.getElementById("minha-lista");
+
+      minhaLista.innerHTML += `<li>${meuInput.value}</li>`;
+      meuInput.value = "";
+      break;
+
+    case divterca:
+      const minhalista2 = document.getElementById("minha-lista2");
+
+      minhaLista2.innerHTML += `<li>${meuInput.value}</li>`;
+      meuInput.value = "";
+      break;
+    case divquarta:
+      const minhaLista3 = document.getElementById("minha-lista3");
+
+      minhaLista3.innerHTML += `<li>${meuInput.value}</li>`;
+      meuInput.value = "";
+      break;
+    case divquinta:
+      const minhaLista4 = document.getElementById("minha-lista4");
+
+      minhaLista4.innerHTML += `<li>${meuInput.value}</li>`;
+      meuInput.value = "";
+      break;
+    case divsexta:
+      const minhaLista5 = document.getElementById("minha-lista5");
+
+      minhaLista5.innerHTML += `<li>${meuInput.value}</li>`;
+      meuInput.value = "";
+      break;
+    case divsabado:
+      const minhaLista6 = document.getElementById("minha-lista6");
+
+      minhaLista6.innerHTML += `<li>${meuInput.value}</li>`;
+      meuInput.value = "";
+      break;
+    case divdomingo:
+      const minhaLista7 = document.getElementById("minha-lista7");
+
+      minhaLista7.innerHTML += `<li>${meuInput.value}</li>`;
+      meuInput.value = "";
+      break;
+    case "":
+      break;
+    default:
+  }
+}
+
+
+
 // let cliques = 0;
 
 // function adicionaTarefa() {
@@ -36,75 +102,3 @@
 //     meuInput3.value = "";
 //   }
 // }
-
-
-let clique = 0
-
-function adicionaTarefa() {
-    const meuInput = document.getElementById("meu-input");
-
-
-let diaEscolhido = document.getElementById("sem")
-
-switch (diaEscolhido) {
-    case Segunda-feira:
-        const minhaLista = document.getElementById("minha-lista");
-
-    minhaLista.innerHTML += `<li>${meuInput.value}</li>`;
-    meuInput.value = "";
-        break
-
-    case "Terca-feira":
-        const minhalista2 = document.getElementById("minha-lista");
-
-        minhaLista2.innerHTML += `<li>${meuInput.value}</li>`;
-        meuInput.value = "";
-        break
-    case "Quarta-feira":
-        const minhaLista3 = document.getElementById("minha-lista");
-
-        minhaLista.innerHTML += `<li>${meuInput.value}</li>`;
-        meuInput.value = "";
-        break
-    case "Quinta-feira":
-        const minhaLista4 = document.getElementById("minha-lista");
-
-    minhaLista.innerHTML += `<li>${meuInput.value}</li>`;
-    meuInput.value = "";
-        break
-    case "Sexta-feira":
-        const minhaLista5 = document.getElementById("minha-lista");
-
-        minhaLista.innerHTML += `<li>${meuInput.value}</li>`;
-        meuInput.value = "";
-        break
-    case "Sabado":
-        const minhaLista6 = document.getElementById("minha-lista");
-
-        minhaLista.innerHTML += `<li>${meuInput.value}</li>`;
-        meuInput.value = "";
-        break
-    case "Domingo":
-        const minhaLista7 = document.getElementById("minha-lista");
-
-        minhaLista.innerHTML += `<li>${meuInput.value}</li>`;
-        meuInput.value = "";
-        break
-    case "":
-       
-        break
-        default:
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-

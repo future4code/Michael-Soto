93ea8c157ac11105f6ab33bb755213c9c255fb37
papirusgx <michael_nickolas@hotmail.com>
2020-10-09 16:40:24 -0300

    exercicio de funções feito(incompleto)

--- semana4/lista-de-exercicios/arquivo.js
+++ semana4/lista-de-exercicios/arquivo.js
@@ -1,38 +1,155 @@
-1 Crie uma função com um valor(parametro)
-2 Crie uma const  cotacao que digite um valor e o transforme em um numero
-3 Retorne o valor do parametro * constante que foi digitada
-4 Crie uma const meuDinheiro que armazene a função e a execute
-5 Imprima no console a constante meuDinheiro
+// INTERPRETAÇÃO DE CÓDIGOS     
+// 1.
 
-100 * (Numero que foi digitado pelo usuario) Ex >>> 100 * (10) = 100
-// 
+//  Crie uma função com um valor(parametro)
+//  Crie uma const  cotacao que digite um valor e o transforme em um numero
+//  Retorne o valor do parametro * constante que foi digitada
+//  Crie uma const meuDinheiro que armazene a função e a execute
+//  Imprima no console a constante meuDinheiro
 
-1 Crie uma funcao com 2 valores(parametro)
-2 Crie uma let valorAposInvestimento
-3 Crie uma switch com let valorAposInvestimento armazenado
-4 Crie 4 case e caso seja uma delas execute seu determinado comando e pause-o
-5  Crie um default ,caso não seja definido nenhum case execute-o e pause-o
-6 Retorne o let valorAposInvestimento
-7  Crie uma const novoMutante que armazene a função e a execute
-8  Crie uma const segundoMontante que armazene a função e a execute
-9 Imprima no console a const novoMutante e segundoMontante
-
-165
-Alert >>> TIPO DE INVESTIMETO INFORMADO INCORRETO
-// 
+// 100 * (Numero que foi digitado pelo usuario) Ex >>> 100 * (10) = 100
 
 
-1 Crie uma const numeros e armazene varios array dentro dele
-2 Crie uma const arra1 vazia
-3 Crie uma const array2 vazia
-4 Crie um for of e aplique uma condição caso seja true execute o comando senão execute outro comando
-5 Imprima a quantidade arrays dentro da cons numeros
-6 Imprima os valores dentro do array1 e array2
+// 2.
+// Crie uma funcao com 2 valores(parametro)
+//  Crie uma let valorAposInvestimento
+//  Crie uma switch com let valorAposInvestimento armazenado
+//  Crie 4 case e caso seja uma delas execute seu determinado comando e pause-o
+//   Crie um default ,caso não seja definido nenhum case execute-o e pause-o
+//  Retorne o let valorAposInvestimento
+//   Crie uma const novoMutante que armazene a função e a execute
+//   Crie uma const segundoMontante que armazene a função e a execute
+//  Imprima no console a const novoMutante e segundoMontante
 
-Quantidade total de números, 14
-6
-8
-// 
+// 165
+// Alert >>> TIPO DE INVESTIMETO INFORMADO INCORRETO
 
+
+// 3.//  Crie uma const numeros e armazene varios array dentro dele
+//  Crie uma const arra1 vazia
+//  Crie uma const array2 vazia
+//  Crie um for of e aplique uma condição: caso seja true execute o comando senão execute outro comando
+//  Imprima a quantidade de  array dentro da const numeros
+//  Imprima a quantida de array dentro da const: array1 e array2
+
+// Quantidade total de números, 14
+// 6
+// 8
+
+
+// 4.
+//  Crie uma const numeros e armazene varios array dentro dele
+//  Crie uma let numero1 com valor Infinity
+//  Crie uma let numero2 com valor 0
+//  Crie um for of e aplique uma condição: caso seja true execute o comando,aplique outra condição caso seja esse execute-o
+//  Imprima a let numero1 e numero2
+
+// 25,12,55,64,121,44,11,84,51,48,14,73,111,283,1,99,13,31,83,131,1,1.1,-10,25
+//  25,12,55,64,121,44,11,84,51,48,14,73,111,283,1,99,13,31,83,131,1,1.1,25
+
+// LOGICA DE PROGRAMAÇÃO
+// 1.
+// const array = [1,2,3,4,5]
+// console.log( array)
+
+
+// const listas = [3, 4, 89, 56, 31, 48, 1, 100]
+// for (let lista of listas) {
+//     console.log(lista)
+// } 
+
+//   const nota = [3, 4, 89, 56, 31, 48, 1, 100]
+//   i=0
+//   while( i< nota.length ) {
+// console.log(nota[i])
+//     i++
+//   }
+
+// 2.
+// a) false
+// b) false
+// c)  true
+// d) true 
+// e)  true
+
+// 3.
+
+
+
+// const quantidadeDeNumerosPares = [0, 2, 4, 8, 10,12]       INCOMPĹETO
+// i = 0
+// while (i < quantidadeDeNumerosPares.length) {
+//   if (quantidadeDeNumerosPares % 2 === 0) {
+//     console.log(quantidadeDeNumerosPares[i])
+//   }
+//   console.log('oekdfjei')
+//   i++
+// }
+
+// 4. 
+
+// function triangulo(a, b, c) {   INCOMPLETO 
+//   const valortriangulo = prompt('Digite o 1º valor do triangulo')
+//   const valortriangulo2 = prompt('Digite o 2º valor do triangulo')
+//   const valortriangulo3 = prompt('Digite o 3º valor do triangulo')
+
+//   if (valortriangulo === valortriangulo2 && valortriangulo !== valortriangulo3 || valortriangulo3 !== valortriangulo2) {
+//     console.log('O triangulo é isosceles')
+//   } else if (valortriangulo !== valortriangulo2 && valortriangulo !== valortriangulo3 || valortriangulo3 !== valortriangulo2) {
+//     console.log('O triangulo é escaleno')
+//   } else {
+//     console.log('O triangulo é equilatero')
+//   }
+
+// }
+
+// console.log(triangulo(3, 3, 3))
+
+// 5.
+
+//   const numero = 15
+//   const numero2 = 30
+//   if(numero > numero2){
+  
+//     console.log('O maior é: ',numero)
+//   }else{
+//     console.log('O maior é: ',numero2)
+//   }
+
+//   const valor = 15
+//   const valor2 = 30
+//   if(valor2 % valor === 0){
+//     console.log(valor + ' não é divisivel por ' + valor2)
+//     console.log(valor2 + '  é divisivel por ' + valor)
+//   }else{
+//     console.log(valor + '  é divisivel por ' + valor2)
+//     console.log(valor2 + ' não  é divisivel por ' + valor)
+//   }
+// console.log('A diferença entre eles é:',valor2 - valor)
+
+
+// FUNÇÕES
+// 1.
+
+// const numeros = [0,1,2,3,4,5,6,7,8,9,10]
+// var text = " ";
+// let i
+// for(i=0; i < numeros.length; i++){
+//   console.log(numeros) 
+//   break 
+    
+// }
+
+// text += numeros[i] ;
+//   const pegar = numeros.splice("")
+//   console.log(text)
+
+// 2.
+// function valor(){
+// alert("Hello World")
+
+// }
+
+// console.log(valor())
 
 

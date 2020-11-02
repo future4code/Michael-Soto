baa3bb23528fbccfb284da7cc1e70944a2b624bd
Michael Soto <michael_nickolas@hotmail.com>
2020-11-02 18:15:42 -0300

    refiz meu codigo

--- semana7/playlist/src/components/UserListPage.js
+++ semana7/playlist/src/components/UserListPage.js
@@ -1,3 +1,4 @@
+
 import axios from "axios";
 import React from "react";
 import styled from "styled-components";
@@ -7,69 +8,66 @@ color: red;
 margin-left: 10px;
 `
 
-
 const baseUrl =
-  "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
+"https://us-central1-labenu-apis.cloudfunctions.net/labefy/playlists";
 
 const axiosConfig = {
-  headers: {
-    Authorization: "michael-soto-dumont",
-  },
+headers: {
+Authorization: "michael-soto-dumont",
+},
 };
 
 class UserListPage extends React.Component {
-  state = {
-    usersList: [  ]
+
+
+componentDidMount = () => {
+this.pegarPlaylist();
+};
+pegarPlaylist = () => {
+axios.post(baseUrl, axiosConfig)
+.then((resposta) => {
+this.setState({ playlists: resposta.data.result.list });
+})
+.catch((error) => {
+console.log(error.message);
+});
+};
+
+criarPlaylist = () => {
+  const body = {
+    name: this.state.playlistValue
   };
-
-  componentDidMount = () => {
-    this.getAllUsers();
-  };
-  getAllUsers = () => {
-     // ESSA É A SINTAXE DO ASYNC E AWAIT (COLOQUE UM ASYNC NA FUNCAO GETALLUSERS)
-
-    // try {
-    //   const response = await axios.get(baseUrl, axiosConfig)
-
-    //   this.setState({ userList: response.data });
-    // } catch (error) {
-    //   console.log(error.message)
-    // }
-
-    axios(baseUrl, axiosConfig)
-      .then((resposta) => {
-        this.setState({ usersList: resposta.data });
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  };
-
-  deleteUser = (userId) => {
-    axios
-    .delete(`${baseUrl}/${userId}`,  axiosConfig)
-      .then((resposta) => {
-        alert("Usuário deletado com sucesso!");
-        this.getAllUsers();
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  };
-  render() {
-    const renderUserList = this.state.usersList.map((usuario) => {
-      return (
-        <p key={usuario.id}>
-          {usuario.name}
-          <DeleteButton onClick={() => this.deleteUser(usuario.id)}>
-            X
-          </DeleteButton>
-        </p>
-      );
-    });
-
-    return <div>{renderUserList}</div>;
-  }
 }
 
+deleteUser = (userId) => {
+axios
+.delete(`${baseUrl}/${userId}`, axiosConfig)
+.then((resposta) => {
+alert("Usuário deletado com sucesso!");
+this.pegarPlaylist();
+})
+.catch((error) => {
+console.log(error.message);
+});
+};
+
+render() {
+const renderedPlaylists = this.state.playlists.map((playlist) => {
+return (
+<p key={playlist.id}>{playlist.name}
+<DeleteButton onClick={() => this.deleteUser(playlist.id)}>
+X
+</DeleteButton>
+</p>
+);
+});
+
+return <div>{renderedPlaylists}</div>;
+}
+}
+
+
 export default UserListPage;
+
+
+ 	
